c9d04025765cf7bdd295c60bba4107235ae2ce20
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2021-10-18 15:42:38 +0000

    Update SkillTest.java

--- src/test/java/duel/SkillTest.java
+++ src/test/java/duel/SkillTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
-public class AptitudesTest 
+public class SkillTest 
 {
 	//Test des aptitudes globales
 	@Test
